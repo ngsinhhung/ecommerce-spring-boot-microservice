31e92b5dcf33f4c21274fd65dfc18ad223dc1019
Sinh Hùng <sinhhung.ng@gmail.com>
2024-07-22 15:20:29 +0700

    product-service: create new product

---
+++ product-service/src/main/java/com/nsh/microservices/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.nsh.microservices.product.controller;
+
+import com.nsh.microservices.product.dto.ProductRequest;
+import com.nsh.microservices.product.dto.ProductResponse;
+import com.nsh.microservices.product.model.Product;
+import com.nsh.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
